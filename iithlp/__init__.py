0162330c3fceaeee76c2493beeb20ed497b49e36
Ramanan <ramanan93@gmail.com>
2025-04-25 16:52:24 +0800

    Updated version

--- iithlp/__init__.py
+++ iithlp/__init__.py
@@ -1,10 +1,10 @@
 """
 IITHLP - Indian Institute of Technology Hyderabad Language Processing
-Version 0.2.0
+Version 0.2.1
 
 A comprehensive transliteration scheme for Indian languages with a strict one-to-one character mapping.
 """
 
 from .core import IITHLP, to_roman, to_script, get_supported_scripts, get_script_info
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="iithlp",
-    version="0.2.0",
+    version="0.2.1",
     author="Ramanan S",
     author_email="ramanan93@gmail.com",
     description="A comprehensive transliteration scheme for Indian languages",
